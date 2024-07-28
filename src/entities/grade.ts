dd8015067fd71448e38aa00ba551e052d6e1cd13
Shibatch <lawnbone.tubist@gmail.com>
2024-07-29 00:45:12 +0900

    add: 学年の型定義を追加

---
+++ src/entities/grade.ts
@@ -0,0 +1,12 @@
+enum Grade {
+    B4 = "B4",
+    M1 = "M1",
+    M2 = "M2",
+    D1 = "D1",
+    D2 = "D2",
+    D3 = "D3",
+    TEACHER = "TEACHER",
+    OBOG = "OB/OG",
+}
+
+export default Grade;

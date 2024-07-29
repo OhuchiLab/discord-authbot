43c017548be6d1179c776b77e482706dd2edb5bd
Shibatch <lawnbone.tubist@gmail.com>
2024-07-29 10:32:32 +0900

    feat: メンバ追加時のイベントを追加

---
+++ guildMemberAdd.ts
@@ -0,0 +1,32 @@
+import { Events, Guild, GuildMember, Role } from "discord.js";
+import { CustomClient } from "./src/types/customClient";
+import createRoleIfNotFound from "./src/utils/createRoleNotFound";
+import unAuthorizedRoleProperty from "./src/roles/unAuthorized";
+
+export function setupGuildMemberAddHandler(client: CustomClient) {
+  client.on(Events.GuildMemberAdd, async (member: GuildMember) => {
+    await sendDirectMessage(member);
+    await giveUnauthorizedRole(member);
+  });
+}
+
+async function sendDirectMessage(member: GuildMember) {
+  try {
+    await member.send(`ようこそ${member.displayName}さん！ 大内研究室 Discord 認証Botです！`);
+    await member.send("名前(フルネーム)を教えてください。");
+    console.log(`Sent a welcome message to ${member.displayName}`);
+  } catch (error) {
+    console.error(`Failed to send a welcome message to ${member.displayName}: ${error}`);
+  }
+}
+
+async function giveUnauthorizedRole(member: GuildMember) {
+  try {
+    const guild: Guild = member.guild;
+    const role: Role = await createRoleIfNotFound({ guild, customRole: unAuthorizedRoleProperty });
+    await member.roles.add(role);
+    console.log(`Added Unauthorized role to ${member.displayName}`);
+  } catch (error) {
+    console.error(`Failed to add Unauthorized role to ${member.displayName}: ${error}`);
+  }
+}

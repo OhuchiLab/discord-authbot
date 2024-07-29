a5433a624bdee05cc37f3207fd6ef652c132c9c3
Shibatch <lawnbone.tubist@gmail.com>
2024-07-29 12:31:55 +0900

    feat: 応答機能を持たせる函数を追加

---
+++ src/events/interactionCreate.ts
@@ -0,0 +1,25 @@
+import { Events } from "discord.js";
+import { CustomClient } from "../types/customClient";
+import { reauthenticateWithCredential } from "firebase/auth";
+
+export function setupInteractionCreateHandler(client: CustomClient) {
+  client.on(Events.InteractionCreate, async (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+    if (!command) {
+      console.error(`Command ${interaction.commandName} not found`);
+      return;
+    }
+
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      console.error(error);
+      const replyContent = { content: "There was an error while executing this command!", ephemeral: true };
+      interaction.replied || interaction.deferred
+        ? await interaction.followUp(replyContent)
+        : await interaction.reply(replyContent);
+    }
+  });
+}
